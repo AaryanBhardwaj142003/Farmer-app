273427eec1eda987fcaa2a48f22695674392e4f6
Luffyguts <aaryanbvp@gmail.com>
2024-09-20 19:52:53 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <>
+      <input type="text" id="text1" />
+      <label htmlFor="text1">Email</label>
+
+      <input type="text" id="text2"/>
+      <label htmlFor="text2">Password</label>
+
+      <button>Submit</button>
+
+    </>
+  )
+}
+
+export default App
